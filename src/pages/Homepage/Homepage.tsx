c2efcb8d2e4ad2173d7b513eb1df99aac32085f4
Maksim Lukianchikov <feltmax@gmail.com>
2020-04-24 00:50:20 +0600

    Fix styleguide line spacing

--- src/pages/Homepage/Homepage.tsx
+++ src/pages/Homepage/Homepage.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { Typography } from 'antd';
 
 import { Header, Footer, Content } from '../../components/base/Layout';
-
 import ProductList from '../../components/common/ProductList';
 
 const { Title } = Typography;
